4e6ea6b53f69b058fc8f3e1a5d58d723585c5501
abdelmoughite-OUADAZ <71220423+abdlmoughite@users.noreply.github.com>
2026-09-12 21:38:36 +0100

    v1 host

---
+++ dist/assets/i18n-BcCCzEdY.js
@@ -0,0 +1,1 @@
+const m=i=>`${Number(i||0).toLocaleString("fr-MA",{minimumFractionDigits:2,maximumFractionDigits:2})} DH`;export{m};
---
+++ dist/assets/package-C2ojKM4Q.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-CnaaiuJw.js";/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=a("Bell",[["path",{d:"M10.268 21a2 2 0 0 0 3.464 0",key:"vwvbt9"}],["path",{d:"M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326",key:"11g9vi"}]]);/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("Package",[["path",{d:"M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z",key:"1a0edw"}],["path",{d:"M12 22V12",key:"d0xqtd"}],["path",{d:"m3.3 7 7.703 4.734a2 2 0 0 0 1.994 0L20.7 7",key:"yx3hmr"}],["path",{d:"m7.5 4.27 9 5.15",key:"1c824w"}]]);export{t as B,c as P};
